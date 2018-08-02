227177c82ff4ee9c239e357faf2d38196436a255
Jeff YANG <j3ffyang@gmail.com>
2018-08-02 16:24:07 +0800

    sample scripts in tutorial

--- scripts/bitwise_and.py
+++ scripts/bitwise_and.py
@@ -1,7 +1,7 @@
 a = input("Give the 1st binary string: ")
 b = input("Give the 2nd binary string: ")
 
-y = int(a,2) & int(b,2)
+y = int(a, 2) & int(b, 2)
 # print('{0:b}'.format(y))
 
 print("The result is: ")
