7c797d1970f2a9a65fed48f79c0419fd966ce30b
Jeff YANG <j3ffyang@gmail.com>
2018-09-23 13:08:50 +0800

    new scrapy proj

--- scripts/scrapy/reddit/reddit/pipelines.py
+++ scripts/scrapy/reddit/reddit/pipelines.py
@@ -5,7 +5,12 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
+from scrapy.exceptions import DropItem
 
 class RedditPipeline(object):
     def process_item(self, item, spider):
-        return item
+
+        if int(item['score']) > 1000:
+            return item
+        else:
+            raise DropItem("too low score")
