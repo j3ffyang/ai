f55d4ff7226917d58d9551daee79c7775811c225
Jeff YANG <j3ffyang@gmail.com>
2018-09-26 22:53:38 +0800

    machine learning prediction

--- scripts/iris_dataset/a3_3_train_test_split.py
+++ scripts/iris_dataset/a3_3_train_test_split.py
@@ -13,7 +13,8 @@ iris = load_iris()
 X = iris.data
 y = iris.target
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=4)
 
 print(X.shape)
 print(X_train.shape)
