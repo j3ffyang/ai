6b36df63270cb05e9bf219bab2781a831d40e383
Jeff YANG <j3ffyang@gmail.com>
2018-09-29 18:17:26 +0800

    cross-validation-score

--- scripts/scikit-learn/cross_val_score.py
+++ scripts/scikit-learn/cross_val_score.py
@@ -14,3 +14,22 @@ print(knn)
 
 scores = cross_val_score(knn, X, y, cv= 10, scoring='accuracy')
 print(scores)
+print(scores.mean())
+
+k_range = range(1, 45)
+k_scores = []
+for k in k_range:
+    knn = KNeighborsClassifier(n_neighbors= k)
+    scores = cross_val_score(knn, X, y, cv= 10, scoring='accuracy')
+    k_scores.append(scores.mean())
+
+print(k_scores)
+
+import matplotlib.pyplot as plt
+# matplotlib inline
+plt.plot(k_range, k_scores)
+plt.xlabel('K value for KNN algorithm')
+plt.ylabel('Mean accuracy scores')
+
+# we can see when x= 12/ 18/ 20, y is highest
+plt.show()
