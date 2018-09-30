74a7ccf2994131e6314f5c69d678a674b6910cf8
Jeff YANG <j3ffyang@gmail.com>
2018-09-30 18:08:37 +0800

    load table by panda

--- scripts/pandas/panda0.py
+++ scripts/pandas/panda0.py
@@ -1,9 +1,17 @@
 import pandas as pd
+import numpy as np
 
 # tsv file downloaded >
 # https://datatables.net/extensions/buttons/examples/html5/tsv.html
+
+cols = ['Name', 'Position']
 employee = pd.read_table('./tab_separated_values.tsv')
-print(employee.head())
+# employee = pd.read_table('./tab_separated_values.tsv', nrows= 4)
+print(employee[cols].head())
+# print(employee.head())
+# print(employee.dtypes)
+print(employee.select_dtypes(include = [np.number]).dtypes)
+print(employee.describe())
 
 # csv file downloaded from https://data-gov.tw.rpi.edu/wiki/CSV_files_use_delimiters_other_than_commas
 # download from > https://www.nrc.gov/reading-rm/doc-collections/event-status/reactor-status/2009/2009PowerStatus.txt
