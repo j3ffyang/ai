9faa638b3aa58d21c1b935c75dc39a2c8b204b7b
Jeff YANG <j3ffyang@gmail.com>
2018-12-04 17:38:28 +0800

    numpy to analyze wine quality

---
+++ scripts/numpy/34empty_n_random_array.py
@@ -0,0 +1,8 @@
+import numpy as np 
+
+empty_array= np.zeros((3, 4))
+print(empty_array)
+
+x= np.random.rand(3, 4)
+print(x)
+
---
+++ scripts/numpy/35data_proc.py
@@ -0,0 +1,17 @@
+# https://www.dataquest.io/blog/numpy-tutorial-python/
+
+import numpy as np 
+
+# data= np.genfromtxt("winequality-red.csv", dtype=float, delimiter=',', names=True)
+data= np.genfromtxt("winequality-red.csv", dtype=float, delimiter=';', skip_header=1)
+
+# print(data.shape)
+print(data[2, 3])   # row 3 and col 4
+print(data[0:3, 3]) # first 3 rows and the 4th rows
+# print(data[:3, 3])    # same the above one 
+
+print(data[:, 2])   # entire 3rd col / column
+print(data[2, :])   # entire 3rd row
+
+print(data)
+
