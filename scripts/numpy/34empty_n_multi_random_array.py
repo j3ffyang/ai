b950e965a35296dee58907ce74f68f95bc26261f
Jeff YANG <j3ffyang@gmail.com>
2018-12-04 17:52:59 +0800

    numpy to analyze wine quality

--- scripts/numpy/34empty_n_multi_random_array.py
+++ scripts/numpy/34empty_n_multi_random_array.py
@@ -3,6 +3,14 @@ import numpy as np
 empty_array= np.zeros((3, 4))
 print(empty_array)
 
+print("=-=-=-=-")
+
 x= np.random.rand(3, 4) # create an array of 3 rows and 4 cols
 print(x)
+
+print("=-=-=-=-")
+
+y= np.random.rand(2,3,4,5)  # create an array in 4 dimensions
+print(y)
+print(y.shape)
 
