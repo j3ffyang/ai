a62e4f8cf1003e85ad5ac318084839f39fe5d967
Jeff YANG <j3ffyang@gmail.com>
2018-12-05 14:24:29 +0800

    plot idx

--- scripts/idx/10pandas_array_plot_single_date_success.py
+++ scripts/idx/10pandas_array_plot_single_date_success.py
@@ -1,25 +1,14 @@
 import pandas as pd 
 import matplotlib.pyplot as plt
 
-df= pd.read_csv('20181128_idx.csv', sep=',', header=None)
+df= pd.read_csv('20181129_polution.csv', sep=',', header=None)
 # data= np.genfromtxt("20181128_idx.csv", dtype=float, delimiter=',', skip_header=1)
 
 # arr= df.T.values 
-arr= df.values.T
-print(arr)
+# arr= df.values.T 
+arr= df.values
 print(arr.shape)
 
-x0= (arr[0])        # all col of date, which is the 1st col
-x= x0[1:]           # from 2nd col 
-print(x)
+date= arr[:, 0][1:] # get col0 then get col1 & after
+print(date)
 
-six0= (arr[1][1:])  # 1= the 2nd row in arr. 1:= from 2nd col to the end
-six= pd.to_numeric(six0)    # change datatype from string to num 
-print(six)          # all col of six 
-
-nine= pd.to_numeric(arr[2][1:]) # 2= 3rd row
-print(nine)
-
-# fig= plt.figure(figsize=(12, 4))
-# plt.plot(x, six, marker='o')
-# plt.show()
