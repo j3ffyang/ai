5bd7faa7d1f5aadf821840a91cdf196edaa1dd29
Jeff YANG <j3ffyang@gmail.com>
2018-12-05 14:30:25 +0800

    plot idx

--- scripts/idx/10pandas_array_plot_single_date_success.py
+++ scripts/idx/10pandas_array_plot_single_date_success.py
@@ -7,8 +7,12 @@ df= pd.read_csv('20181129_polution.csv', sep=',', header=None)
 # arr= df.T.values 
 # arr= df.values.T 
 arr= df.values
+print(arr)
 print(arr.shape)
 
-date= arr[:, 0][1:] # get col0 then get col1 & after
+date= arr[:, 0][1:]     # get col0 then get col1 & after
 print(date)
+
+data= arr[:, 1:][1:]    # get all from 2nd col (col1) and remove row0
+print(data)
 
