63db63a0cbdf3e5cf73e2a3cc8656770c2b1e743
Jeff YANG <j3ffyang@gmail.com>
2018-12-05 11:06:11 +0800

    plot idx

--- scripts/idx/09pandas_array_plot_single_date_success.py
+++ scripts/idx/09pandas_array_plot_single_date_success.py
@@ -1,7 +1,5 @@
-import numpy as np 
 import pandas as pd 
 import matplotlib.pyplot as plt
-# from matplotlib import pyplot
 
 df= pd.read_csv('20181128_idx.csv', sep=',', header=None)
 # data= np.genfromtxt("20181128_idx.csv", dtype=float, delimiter=',', skip_header=1)
@@ -10,15 +8,14 @@ df= pd.read_csv('20181128_idx.csv', sep=',', header=None)
 arr= df.values.T
 print(arr.shape)
 
-x0= (arr[0])         # all col of date, which is the 1st col
+x0= (arr[0])        # all col of date, which is the 1st col
 x= x0[1:]           # from 2nd col 
 print(x)
 
-# six= (arr[1])
-six0= (arr[1][1:])   # 1= the 2nd row in arr. 1:= from 2nd col to the end
+six0= (arr[1][1:])  # 1= the 2nd row in arr. 1:= from 2nd col to the end
 six= pd.to_numeric(six0)    # change datatype from string to num 
 print(six)          # all col of six
 
-fig= plt.figure(figsize=(12, 4))    # 
+fig= plt.figure(figsize=(12, 4))
 plt.plot(x, six, marker='o')
 plt.show()
