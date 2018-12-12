075bfefeb4132fb903a41ba46dbcb7591037e636
Jeff YANG <j3ffyang@gmail.com>
2018-12-12 16:46:40 +0800

    more code to predict by scikit

--- scripts/scikit-learn/12predict_bikebuy.py
+++ scripts/scikit-learn/12predict_bikebuy.py
@@ -12,7 +12,17 @@ array= dataframe.values
 X= array[:, 0:11]
 Y= array[:, 11]
 
+# print(X)
+# print(Y)
+
 test_size= .30
 seed= 45
 
+X_train, X_test, Y_train, Y_test= train_test_split(X, Y, test_size=test_size, random_state=seed)
 
+model= RandomForestClassifier()
+model.fit(X_train, Y_train)
+ 
+result= model.score(X_test, Y_test)
+
+print(("Accuracy: %.3f%%") % (result* 100.0))
