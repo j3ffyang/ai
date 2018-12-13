8104d5f39513c333fd9e33bebbeedbba068d4d70
Jeff YANG <j3ffyang@gmail.com>
2018-12-13 11:25:31 +0800

    tuned prediction by minmaxscaler with kNN

--- scripts/scikit-learn/22loan_pred_tuned_pred.py
+++ scripts/scikit-learn/22loan_pred_tuned_pred.py
@@ -27,10 +27,31 @@ result= accuracy_score(Y_test, knn.predict(X_test[['ApplicantIncome',
     'Credit_History']]))
 
 # print(result)
+print('Original accuracy result without being tuned')
 print(("Accuracy: %.3f%%") % (result* 100.0))
+print('########')
 
 print('Loan_Status in train dataset. In result, 70% of approved loans')
 print(Y_train.Target.value_counts()/ Y_train.Target.count()) 
 
-print('Use the above train dataset to predict for "test"')
+print('Use the above train dataset to predict for "test" of Loan_Status')
 print(Y_test.Target.value_counts()/ Y_test.Target.count())
+print('########')
+
+# tuning modeling by Scaling
+# importing MinMaxScaler and initializaing it 
+from sklearn.preprocessing import MinMaxScaler
+min_max= MinMaxScaler()
+
+# scaling down both train and test dataset
+X_train_minmax= min_max.fit_transform(X_train[['ApplicantIncome', 'CoapplicantIncome', 'LoanAmount', 'Loan_Amount_Term', 'Credit_History']])
+
+X_test_minmax= min_max.fit_transform(X_test[['ApplicantIncome', 'CoapplicantIncome', 'LoanAmount', 'Loan_Amount_Term', 'Credit_History']])
+
+# fitting kNN on scaled dataset 
+knn= KNeighborsClassifier(n_neighbors= 5)
+knn.fit(X_train_minmax, Y_train.values.ravel())
+
+result= accuracy_score(Y_test, knn.predict(X_test_minmax))
+print('After tuning')
+print(("Accuracy: %.3f%%") % (result* 100.0))
