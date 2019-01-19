63490e007771b9a399295ee7a7f8c429f2df317a
Jeff YANG <j3ffyang@gmail.com>
2019-01-19 23:27:52 +0800

    test and training dataset spliting, ml tutorial

--- scripts/ml/51.py
+++ scripts/ml/51.py
@@ -1,7 +1,7 @@
 from sklearn.datasets import load_iris
 
 iris= load_iris()
-x= iris.data 
+x= iris.data
 y= iris.target
 
 from sklearn.linear_model import LogisticRegression
@@ -22,7 +22,35 @@ print(knn.fit(x, y))
 print(x.shape)
 print(y.shape)
 
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
+print(x.shape)
+print(y.shape)
+
 x_train, x_test, y_train, y_test= train_test_split(x, y, test_size= 0.4,
         random_state= 4)
+print(x_test.shape)
 print(x_train.shape)
+print(y_test.shape)
+print(y_train.shape)
+
+logreg= LogisticRegression()
+logreg.fit(x_train, y_train)
+y_pred= knn.predict(x_test)
+print(metrics.accuracy_score(y_test, y_pred))
+
+k_range= range(1, 26)
+scores= []
+for k in k_range:
+    knn= KNeighborsClassifier(n_neighbors= k)
+    knn.fit(x_train, y_train)
+    y_pred= knn.predict(x_test)
+    scores.append(metrics.accuracy_score(y_test, y_pred))
+print(scores)
+
+import matplotlib.pyplot as plt
+plt.plot(k_range, scores)
+
+plt.xlabel('k for kNN')
+plt.ylabel('Testing Accuracy')
+
+plt.show()
