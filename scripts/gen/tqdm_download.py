a6669602f6fcd3dff45a15ff356310ffffd9cac1
Jeff YANG <j3ffyang@gmail.com>
2019-12-16 19:20:48 +0800

    tqdm download

--- scripts/gen/tqdm_download.py
+++ scripts/gen/tqdm_download.py
@@ -7,3 +7,11 @@ text = ""
 for char in tqdm(["a", "b", "c", "d"]):
     time.sleep(0.25)
     text = text + char
+
+# for i in trange(100):
+#    time.sleep(0.01)
+
+pbar = tqdm(["a", "b", "c", "d"])
+for char in pbar:
+    time.sleep(0.25)
+    pbar.set_description("Processing %s" % char)
